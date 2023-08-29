bd47fb156b2a9692a54b21efd5b5df8ef8431518
HeewonP825 <7hwon@naver.com>
2023-08-30 03:25:32 +0900

    Feat(MainFragment): add btn click event

--- app/src/main/java/com/hackathon/gameselect/ui/MainFragment.kt
+++ app/src/main/java/com/hackathon/gameselect/ui/MainFragment.kt
@@ -5,8 +5,29 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.hackathon.gameselect.R
+import com.hackathon.gameselect.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
+    private lateinit var binding: FragmentMainBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // 버튼 클릭 시 ResultFragment로 이동
+        binding.startsearachBtn.setOnClickListener {
+            findNavController().navigate(R.id.action_mainFragment_to_resultFragment)
+        }
+    }
 
 }
